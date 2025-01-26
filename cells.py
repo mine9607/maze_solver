f69ed19b574c824910a534b1dfcf39a91c6bbdf9
mine9607 <81383389+mine9607@users.noreply.github.com>
2025-01-26 14:07:12 -0500

    added a draw_move class method in the cell class to represent the line drawn between the center point of two cells

--- cells.py
+++ cells.py
@@ -1,6 +1,7 @@
 from draw import Point, Line
 
 # NOTE: to improve code - refactor the draw_cell function to include a helper function draw_wall
+# NOTE: refactor with a get_center method to get self.get_center() and to_cell.get_center()
 
 class Cell:
   def __init__(self, x1, x2, y1, y2, win, left_wall=True, right_wall=True, top_wall=True, bottom_wall=True ):
@@ -44,3 +45,22 @@ class Cell:
       left_wall_line = Line(start_point, end_point)
       self.__win.draw_line(left_wall_line, fill_color)
 
+  def draw_move(self, to_cell, undo=False):
+    '''
+    this method should draw a line between the CENTER of two cells
+
+    if undo = false draw a red line
+    else draw a gray line
+
+    this allows us to visually see when our line is backtracking across a path previously drawn
+
+    self and to_cell will both be instances of the Cell class and therefore have x/y coordinates
+    '''
+
+    self_center = Point(self.__x1 + (self.__x2 - self.__x1)/2, self.__y1+(self.__y2-self.__y1)/2)
+    to_cell_center = Point(to_cell.__x1 + (to_cell.__x2 - to_cell.__x1)/2, to_cell.__y1 + (to_cell.__y2-to_cell.__y1)/2)
+    
+    connecting_line = Line(self_center, to_cell_center)
+
+    fill_color = "gray" if undo else "red"
+    self.__win.draw_line(connecting_line, fill_color=fill_color)
