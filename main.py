6f9d622a4448a4f50b4d20b443563528c1ca17b9
mine9607 <81383389+mine9607@users.noreply.github.com>
2025-01-26 18:58:59 -0500

    updated main.py to include a main function and variables to define the maze size

--- main.py
+++ main.py
@@ -1,36 +1,18 @@
 from window import Window
-from draw import Point, Line
-from cells import Cell
+from maze import Maze
 
-win = Window(800, 600)
+def main():
+  num_rows = 12
+  num_cols = 16
+  margin = 50
+  screen_x = 800
+  screen_y = 600
+  cell_size_x = (screen_x -2 * margin) / num_cols
+  cell_size_y = (screen_y -2 * margin) / num_rows
+  win = Window(screen_x, screen_y)
 
-#Create points
-point1 = Point(100, 100)
-point2 = Point(300, 100)
-point3 = Point(300, 300)
-point4 = Point(100, 300)
+  maze = Maze(margin, margin, num_rows, num_cols, cell_size_x, cell_size_y, win)
 
-# Create lines connecting the points
-line1 = Line(point1, point2)
-line2 = Line(point1, point3)
-line3 = Line(point1, point4)
-line4 = Line(point4, point2)
+  win.wait_for_close()
 
-win.draw_line(line1, fill_color="red")
-win.draw_line(line2, fill_color="blue")
-win.draw_line(line3, fill_color="green")
-win.draw_line(line4, fill_color="purple")
-
-cell1 = Cell(500, 550, 100, 150, win, top_wall=False) # no top wall
-cell1.draw_cell(fill_color="red")
-
-cell2 = Cell(550, 600, 150, 200, win, right_wall=False) # no right wall
-cell2.draw_cell()
-
-cell3 = Cell(500, 550, 200, 250, win, bottom_wall=False) # no bottom wall
-cell3.draw_cell(fill_color="green")
-
-cell4 = Cell(450, 500, 150, 200, win, left_wall=False) # no left wall
-cell4.draw_cell(fill_color="blue")
-
-win.wait_for_close()
+main()
--- maze.py
+++ maze.py
@@ -24,10 +24,10 @@ class Maze:
 
     Once the matrix is populated it should call its _draw_cell() method on each Cell
     '''
-    for i in range(self.num_cols):
+    for i in range(self._num_cols):
       # note the outer loop should increment shift the start point of the cell x1 by (x1+cell_size*i)
       col_list = []
-      for j in range(self.num_rows):
+      for j in range(self._num_rows):
         # note each pass of the inner loop should should shift the start of the y point
         x1 = self._x1 + self._cell_size_x*i
         x2 = x1 + self._cell_size_x
@@ -36,28 +36,28 @@ class Maze:
         y2 = y1 + self._cell_size_y
 
         # Create a cell object 
-        cell = Cell(x1 = x1, x2 = x2, y1 = y1, y2 = y2,win = self.__win, left_wall=True, right_wall=True, top_wall= True, bottom_wall=True)
+        cell = Cell(x1 = x1, x2 = x2, y1 = y1, y2 = y2,win = self._win, left_wall=True, right_wall=True, top_wall= True, bottom_wall=True)
         col_list.append(cell)
         # self.__draw_cell(i,j)  
         # NOTE: why call self.__draw_cell when the Cell class already has a draw_cell(self) method? - Note because we need to call self.__animate()
         cell.draw_cell()
-        self.__animate()
+        self._animate()
 
-      self.__cells.append(col_list)
+      self._cells.append(col_list)
 
-  def __draw_cell(self, i, j):
-    pass
+  # def __draw_cell(self, i, j):
+  #   pass
 
-    ''' 
-    This method should calculate the x/y position of the Cell based on i, j, the cell_size, and the x/y position of the Maze itself
+  #   ''' 
+  #   This method should calculate the x/y position of the Cell based on i, j, the cell_size, and the x/y position of the Maze itself
 
-    The x/y position of the maze represents how many pixels from the top and left the maze should start from the side of the window
+  #   The x/y position of the maze represents how many pixels from the top and left the maze should start from the side of the window
 
-    Once calculated, it should draw the cell and call the maze's _animate() method
-    '''
-    # Calculate the cell 
+  #   Once calculated, it should draw the cell and call the maze's _animate() method
+  #   '''
+  #   # Calculate the cell 
 
-    self._animate()
+  #   self._animate()
   
   def _animate(self):
     ''' 
